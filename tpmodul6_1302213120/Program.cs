d26631584c590055ba3742505dbee4e9f9fc3763
Walid Hanif Ataullah <walidhnf12@gmail.com>
2023-03-26 09:52:36 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302213120/Program.cs
+++ tpmodul6_1302213120/Program.cs
@@ -10,6 +10,37 @@ namespace tpmodul6_1302213120
     {
         static void Main(string[] args)
         {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Walid Hanif Ataullah");
+            video.PrintVideoDetails();
+        }
+    }
+
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        // Constructor
+        public SayaTubeVideo(string title)
+        {
+            this.id = new Random().Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        // Method to increase play count
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        // Method to print video details
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + this.id);
+            Console.WriteLine("Title: " + this.title);
+            Console.WriteLine("Play count: " + this.playCount);
         }
     }
 }
